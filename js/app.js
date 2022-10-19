1e9cda2650c137ef23f275d394956d6a2b4d04ec
Jeisson Gómez <jeissongomez@hotmail.es>
2022-10-18 22:34:52 -0500

    Elimina un curso del carrito

--- js/app.js
+++ js/app.js
@@ -8,6 +8,9 @@ cargarEventListeners();
 function cargarEventListeners() {
   //Cuando agregas un curso presionando "Agregar al Carrito"
   listaCursos.addEventListener("click", agregarCurso);
+
+  //Elimina cursos del carrito
+  carrito.addEventListener("click", eliminarCurso);
 }
 
 //Funciones
@@ -17,6 +20,18 @@ function agregarCurso(e) {
   if (e.target.classList.contains("agregar-carrito")) {
     const cursoSeleccionado = e.target.parentElement.parentElement;
     leerDatosCurso(cursoSeleccionado);
+  }
+}
+
+//Elimina un curso del carrito
+function eliminarCurso(e) {
+  if (e.target.classList.contains("borrar-curso")) {
+    const cursoId = e.target.getAttribute("data-id");
+
+    //Elimina del arreglo de articulosCarrito por el data-id
+    articulosCarrito = articulosCarrito.filter((curso) => curso.id != cursoId);
+
+    carritoHTML(); //Iterar sobre el carrito y mostrar su HTML
   }
 }
 
